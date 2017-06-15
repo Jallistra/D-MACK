7482af94581f3fb748ee1b3f3c84b6fceb37b720
JoeBeer <c.cetinkaya@live.de>
2017-06-15 16:10:53 +0200

    Fertigstellung der Klasse Date, Erstellung nötiger Klassen mit Variablen und Methoden

---
+++ src/source/HashSetTest.java
@@ -0,0 +1,32 @@
+package source;
+
+import java.util.HashSet;
+
+/**
+ * Created by cceti on 14.06.2017.
+ */
+public class HashSetTest {
+
+    // =========================== Class Variables ===========================79
+    // =============================  Variables  =============================79
+    // ============================  Constructors  ===========================79
+    // ===========================  public  Methods  =========================79
+    public static void main(String args[]) {
+        // create a hash set
+        HashSet hs = new HashSet();
+
+        // add elements to the hash set
+        hs.add("B");
+        hs.add("A");
+        hs.add("D");
+        hs.add("E");
+        hs.add("C");
+        hs.add("F");
+        System.out.println(hs);
+    }
+
+    // =================  protected/package local  Methods ===================79
+    // ===========================  private  Methods  ========================79
+    // ============================  Inner Classes  ==========================79
+    // ============================  End of class  ===========================79
+}
